94884fe46cde9e40868f2c7215fea6287c4a0ba7
ghostxbh <ghostxbh@hotmail.com>
2020-02-12 21:15:03 +0800

    add post callback api

--- routes/sms.js
+++ routes/sms.js
@@ -7,8 +7,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/callback.action', function (req, res, next) {
-    const param = req.body || req.params;
-    console.log(param);
+    const param = req.params || req.header.params;
+    console.log('get: ', param);
+    console.log('get req: ', req);
+    res.status(200).send('delivrd');
+});
+
+router.post('/callback.action', function (req, res, next) {
+    const param = req.body;
+    console.log('post: ', param);
+    console.log('post req: ', req);
     res.status(200).send('delivrd');
 });
 
